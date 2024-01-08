2e2d3c5f6d447935be19be0c01534259a4e18ef0
Alexander Tong <92343744+AlexT2052@users.noreply.github.com>
2024-01-07 21:33:53 -0600

    Add power subsystem (#12)

---
+++ src/main/java/frc/robot/subsystems/PowerSubsystem.java
@@ -0,0 +1,55 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class PowerSubsystem extends SubsystemBase {
+
+  private PowerDistribution m_revPDH;
+
+    private double voltage, temperatureCelsius, totalCurrent, totalPower, totalEnergy, numChannels;
+    private double currentsArray[]; 
+
+  /** Subsystem for controlling the power of the robot */
+  public PowerSubsystem() {
+    m_revPDH = new PowerDistribution(1, ModuleType.kRev);
+
+    currentsArray = new double[20];
+  }
+
+  public void setPower(double power) {} // Set the power of the robot
+
+  @Override
+    public void periodic() {
+        voltage = m_revPDH.getVoltage();
+        temperatureCelsius = m_revPDH.getTemperature(); // Retrieves the temperature of the PDP, in degrees Celsius.
+        totalCurrent = m_revPDH.getTotalCurrent(); // Get the total current of all channels.
+        totalPower = m_revPDH.getTotalPower(); // Get the total power of all channels, the bus voltage multiplied by the current with the units Watts.
+        totalEnergy = m_revPDH.getTotalEnergy(); // Get the total energy of all channels with units Joules.
+
+        SmartDashboard.putNumber("Voltage", voltage);
+        SmartDashboard.putNumber("Total Current", totalCurrent);
+        SmartDashboard.putNumber("Total Power", totalPower);
+        SmartDashboard.putNumber("Total Energy", totalEnergy);
+        SmartDashboard.putNumber("Temperature", temperatureCelsius);
+        
+        numChannels = m_revPDH.getNumChannels();
+
+        // Get the current going through all channels, in Amperes.
+        // The PDP returns the current in increments of 0.125A.
+        // At low currents the current readings tend to be less accurate.
+        for(int i = 0; i < currentsArray.length; i++) {
+            currentsArray[i] = m_revPDH.getCurrent(i);
+        }
+        SmartDashboard.putNumberArray("Currents", currentsArray);
+        SmartDashboard.putNumber("Temperature", numChannels);
+
+        SmartDashboard.putNumber("Left Front Drive Motor Current", currentsArray[Constants.LEFT_FRONT_DRIVE_MOTOR_PDH_CHANNEL]);
+        SmartDashboard.putNumber("Right Front Drive Motor Current", currentsArray[Constants.RIGHT_FRONT_DRIVE_MOTOR_PDH_CHANNEL]);
+        SmartDashboard.putNumber("Left Rear Drive Motor Current", currentsArray[Constants.LEFT_REAR_DRIVE_MOTOR_PDH_CHANNEL]);
+        SmartDashboard.putNumber("Right Rear Drive Motor Current", currentsArray[Constants.RIGHT_REAR_DRIVE_MOTOR_PDH_CHANNEL]);
+    }
+}
